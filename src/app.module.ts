09b1b1f9d888e7d73ec24f9680239b2ac7d0b8ba
egocentryk <lukasz.skowron@gmail.com>
2025-02-28 20:55:21 +0100

    feat(): postgre database & repository setup created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,34 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 import { CoffeesModule } from './coffees/coffees.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { configSchema } from './config.schema'
 
 @Module({
   imports: [
+    CoffeesModule,
+    ConfigModule.forRoot({
+      validationSchema: configSchema,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
-    CoffeesModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: parseInt(configService.get('DATABASE_PORT') || '5432', 10),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common'
 import { CreateCoffeeInput } from './dto/create-coffee.input'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Coffee } from './entities/coffee.entity'
+import { Repository } from 'typeorm'
+import { UserInputError } from '@nestjs/apollo'
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
   async findAll() {
-    return []
+    return this.coffeeRepository.find()
   }
 
   async findOne(id: number) {
-    return null
+    const coffee = await this.coffeeRepository.findOne({ where: { id } })
+
+    if (!coffee) {
+      throw new UserInputError(`Coffee #${id} does not exist`)
+    }
+
+    return coffee
   }
 
   async create(createCoffeeInput: CreateCoffeeInput) {
-    return null
+    const coffee = this.coffeeRepository.create(createCoffeeInput)
+    return this.coffeeRepository.save(coffee)
   }
 }
