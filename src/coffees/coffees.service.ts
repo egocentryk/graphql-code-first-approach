125a06ea20cb6549c4ad328d35f2030f0f53e684
egocentryk <lukasz.skowron@gmail.com>
2025-03-01 12:05:05 +0100

    feat(): adding relation to two entities

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,12 +5,15 @@ import { Coffee } from './entities/coffee.entity'
 import { Repository } from 'typeorm'
 import { UserInputError } from '@nestjs/apollo'
 import { UpdateCoffeeInput } from './dto/update-coffee.input'
+import { Flavor } from './entities/flavor.entity'
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
   async findAll() {
     return this.coffeeRepository.find()
@@ -27,14 +30,29 @@ export class CoffeesService {
   }
 
   async create(createCoffeeInput: CreateCoffeeInput) {
-    const coffee = this.coffeeRepository.create(createCoffeeInput)
+    const flavors = await Promise.all(
+      createCoffeeInput.flavors.map((name) => this.preloadFlavorByName(name)),
+    )
+
+    const coffee = this.coffeeRepository.create({
+      ...createCoffeeInput,
+      flavors,
+    })
+
     return this.coffeeRepository.save(coffee)
   }
 
   async update(id: number, updateCoffeeInput: UpdateCoffeeInput) {
+    const flavors =
+      updateCoffeeInput.flavors &&
+      (await Promise.all(
+        updateCoffeeInput.flavors.map((name) => this.preloadFlavorByName(name)),
+      ))
+
     const coffee = await this.coffeeRepository.preload({
       id,
       ...updateCoffeeInput,
+      flavors,
     })
 
     if (!coffee) {
@@ -47,5 +65,13 @@ export class CoffeesService {
   async remove(id: number) {
     const coffee = await this.findOne(id)
     return this.coffeeRepository.remove(coffee)
+  }
+
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository.findOne({
+      where: { name },
+    })
+
+    return existingFlavor || this.flavorRepository.create({ name })
   }
 }
