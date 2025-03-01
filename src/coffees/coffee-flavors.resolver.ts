625bd048560b0baedb479e7ba7148b561b022d9e
egocentryk <lukasz.skowron@gmail.com>
2025-03-01 17:58:54 +0100

    feat(): field resolvers used

---
+++ src/coffees/coffee-flavors.resolver.ts
@@ -0,0 +1,22 @@
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql'
+import { Coffee } from './entities/coffee.entity'
+import { Flavor } from './entities/flavor.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+@Resolver(() => Coffee)
+export class CoffeeFlavorsResolver {
+  constructor(
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
+  ) {}
+
+  @ResolveField('flavors', () => [Flavor])
+  async getFlavorsOfCoffee(@Parent() coffee: Coffee) {
+    return this.flavorRepository
+      .createQueryBuilder('flavor')
+      .innerJoin('flavor.coffees', 'coffee')
+      .where('coffee.id = :coffeeId', { coffeeId: coffee.id })
+      .getMany()
+  }
+}
